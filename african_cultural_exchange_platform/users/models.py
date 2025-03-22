4d8fe4063d8304680421f99a3150ce400f3a560a
eph-rem007 <ephraimfekadu@gmail.com>
2025-03-22 21:25:39 +0300

    models creation

--- african_cultural_exchange_platform/users/models.py
+++ african_cultural_exchange_platform/users/models.py
@@ -5,13 +5,20 @@ class customUserManager (BaseUserManager):
     def create_user(self, email, password, **kwargs):
         if not email:
             raise ValueError ("Username is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(use=self._db)
+        return user
 
 class userProfiles(AbstractBaseUser):
     FirstName = models.CharField(max_length=150)
     LastName = models.CharField(max_length=150)
-    email = models.CharField(max_length=150)
+    username = models.CharField(max_length=20, unique=True)
+    email = models.CharField(max_length=150, unique=True)
+    date_joined = models.DateField(auto_now_add=True)
     is_staff = models.BooleanField(default=False)
 
     objects = customUserManager
 
-    USER_NAME_FIELD = "email"
+    USERNAME_FIELD = "email"
